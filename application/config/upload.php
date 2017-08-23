b0d3afc062bcf50c7764a8ee61597a2e7cdcf602
Jesus Angel Hernandez de Rojas <jesusangel.hernandez@uva.es>
2017-08-23 09:22:26 +0200

    Update to codeigniter 3.15

--- application/config/upload.php
+++ application/config/upload.php
@@ -7,10 +7,7 @@
 | Configuración de las subidas de ficheros
 |
 */
-//$config['upload_path'] = '/servicio/apache/www/servicios/ebayuva/img_pisos';
 $config['upload_path'] = '/servicios/samba/silos/silo1/aplicaciones/ebayuva/img_pisos';
 $config['allowed_types'] = 'gif|jpg|png|jpeg';
 $config['max_size']	= '20000';
-//$config["max_width"]  = "1024";
-//$config["max_height"]  = "768";
 ?>
