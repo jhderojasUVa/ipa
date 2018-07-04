1e60e151faf63798cfd212b9b40abff002ab06b8
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-07-04 19:24:45 +0200

    A little update

--- application/models/pisos_model.php
+++ application/models/pisos_model.php
@@ -310,7 +310,7 @@ class Pisos_model extends CI_Model {
 		// Funcion que devuelve 5 imagenes de los ultimos pisos añadidos y en caso de que no, la imagen estandar
 //<<<<<<< HEAD
 		//$sql = "SELECT DISTINCT (imagenes_pisos.idpiso), imagenes_pisos.imagen, imagenes_pisos.descripcion FROM imagenes_pisos, pisos WHERE imagenes_pisos.idpiso=pisos.id_piso AND pisos.verificado=true ORDER BY idpiso DESC LIMIT 10";
-		$sql = "SELECT DISTINCT imagenes_pisos.idpiso FROM imagenes_pisos, pisos WHERE imagenes_pisos.idpiso=pisos.id_piso AND pisos.verificado=true AND pisos.libre=1 ORDER BY idpiso DESC, orden LIMIT 10";
+		//$sql = "SELECT DISTINCT imagenes_pisos.idpiso FROM imagenes_pisos, pisos WHERE imagenes_pisos.idpiso=pisos.id_piso AND pisos.verificado=true AND pisos.libre=1 ORDER BY idpiso DESC, orden LIMIT 10";
 		//$sql = "SELECT DISTINCT imagenes_pisos.idpiso FROM imagenes_pisos, pisos WHERE imagenes_pisos.idpiso = pisos.id_piso AND pisos.verificado=TRUE AND pisos.libre=1 ORDER BY imagenes_pisos.idpiso LIMIT 10";
 //=======
 		$sql = "SELECT DISTINCT imagenes_pisos.idpiso FROM imagenes_pisos, pisos WHERE imagenes_pisos.idpiso = pisos.id_piso AND pisos.verificado=TRUE AND pisos.libre=1 ORDER BY imagenes_pisos.idpiso LIMIT 10";
