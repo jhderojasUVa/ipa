fb246fd4170e2704b0de8f3243a06b9e7b8ad998
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-07-11 12:30:15 +0200

    Componente slideshow beta 0

---
+++ application/controllers/components/slideshow.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Slideshow extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		// Carga de librerias y demas
+		// Helpers
+		$this -> load -> helper("url");
+		$this->load->helper("file");
+
+		// Librerias
+		$this -> load -> library("sesiones_usuarios");
+		$this -> load -> library("SSOUVa");
+		$this -> load -> library("LDAP");
+
+		$config["upload_path"] = "/Volumes/320Gb/httpdocs/ebayuva2/img_pisos";
+		$config["allowed_types"] = "gif|jpg|png";
+		$config["max_size"]	= "20000";
+
+		// Y la libreria
+		$this -> load -> library("upload", $config);
+		$this -> load -> library("pagination");
+	}
+
+	public function index() {
+		echo "Esta pagina no se puede cargar directamente";
+		echo "<script>window.location.href('/');</script>";
+	}
+
+  public function slideshow() {
+    // Funcion que devuelve el JSON necesario para el componente del slideshow
+
+    $datos["ultimos_6"] = $this -> pisos_model -> muestra_5_imagenes_piso(6);
+
+    // Cambiamos la cabecera a JSON de respuesta
+    header('Content-Type: application/json');
+    // Escupimos la respuesta
+    echo json_encode($datos);
+
+  }
+
+}
---
+++ js/components/slideshowComponent.js
@@ -0,0 +1,41 @@
+// Slideshow Component
+
+class SlideshowComponent extends React.Component {
+
+  contructor(props) {
+    super(props);
+    this.state = {
+      ultimos_6: [],
+      isloading: true,
+    }
+  }
+
+  componentWillMount() {
+    return fetch ('/index.php/componentes/slideshow/slideshow')
+            .then((response) => reponse.json())
+            .then((responsejson) => {
+              this.setState({
+                isloading: false,
+                ultimos_6: responsejson.ultimos_6
+              });
+            })
+            .catch((error) => {
+              alert('Oh!\n\rHa habido un error al cargar el JSON del slideshow');
+              throw new Error('Ha habido un error al cargar el JSON del slideshow:\n\r' + error);
+            })
+  }
+
+  render() {
+    if (this.state.isloading === false) {
+      // Si ya ha cargado
+      return (
+      );
+    } else {
+      // Si esta cargando
+      return (
+      );
+    }
+  }
+}
+
+ReactDOM.render(<SlideshowComponent />, document.getElementById('slideshowcomponent'));
