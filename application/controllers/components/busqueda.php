4c869f2ed84f2751b0840b62eb09848db61f3cb7
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-11-21 13:05:24 +0100

    Busquedas solo personal UVa
    Ajustado para que las busquedas sean solo de personal UVa y al resto les ponga un mensaje en el JSON. Asi la vista es mucho mas sencilla.

--- application/controllers/components/busqueda.php
+++ application/controllers/components/busqueda.php
@@ -32,9 +32,6 @@ class Busqueda extends CI_Controller {
 		// Usaremos el metodo spotify para los generos para buscar por ciudades y barrios
 		// es decir barrio:ID o barrio:nombre
 
-		// Por ahora la limpia de barios y ciudades... mas adelante
-		// step by step
-
 		$datos["q"] = $this -> input -> post_get('q');
 
 		// Sacamos las ciudades y los barrios
@@ -62,6 +59,25 @@ class Busqueda extends CI_Controller {
 		} else {
 			// Sino sacamos la cantidad
 			$datos["total"] = sizeof($datos["resultados"]);
+		}
+
+		// Vamos a la autentificacion, si es UVa tendra todo, asi que discriminamos
+		// y si no es, limpiamos todo para que reciba nada
+		if ($this -> sesiones_usuarios -> esta_logeado() == true) {
+			if ($_SESSION["uva"] != true) {
+				// Si NO es UVa
+				$datos["resultados"][$i]["descripcion"] = "Lo sentimos. Solo los usuarios autentificados de la UVa pueden ver toda la informacion.";
+				$datos["resultados"][$i]["direccion"] = "Lo sentimos. Solo los usuarios autentificados de la UVa pueden ver toda la informacion.";
+				$datos["resultados"][$i]["cp"] = "";
+			}
+		} else {
+			// O no esta autentificado
+			//$datos["resultados"] = false;
+			for ($i = 0; $i < sizeof($datos["resultados"]); $i++) {
+				$datos["resultados"][$i]["descripcion"] = "Lo sentimos. Solo los usuarios autentificados de la UVa pueden ver toda la informacion.";
+				$datos["resultados"][$i]["direccion"] = "Lo sentimos. Solo los usuarios autentificados de la UVa pueden ver toda la informacion.";
+				$datos["resultados"][$i]["cp"] = "";
+			}
 		}
 
 		// Cambiamos la cabecera a JSON de respuesta
