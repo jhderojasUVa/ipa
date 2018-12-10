6bca7e586b81917b9307486ea8d3276294b17801
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-12-10 09:17:44 +0100

    Cambiado el React de añadir a produccion
    Cambiado el src del js de añadir piso a produccion

--- application/views/mis/add.php
+++ application/views/mis/add.php
@@ -1,3 +1,3 @@
 <? $this -> load -> helper("url"); ?>
 <div id="addpiso"></div>
-<script type="text/babel" src="<?=base_url()?>js/components/addPisoComponent.js"></script>
+<script src="<?=base_url()?>js/components/production/addPisoComponent.js"></script>
